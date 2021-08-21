14b47be2190bc4f484c732bd4bd09b6c2254f46f
JorshuaG <joshbgearheart@gmail.com>
2021-08-20 14:23:23 -1000

    finished interview questions

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Color from './Color';
-
+import Color from "./Color";
+const testColor = {
+  color: "red",
+  code: { hex: "#225fbg" },
+  id: 42,
+};
 test("Renders without errors with blank color passed into component", () => {
+  render(<Color />);
 });
-  
+
 test("Renders the color passed into component", () => {
+  render(<Color color={testColor} />);
 });
 
-test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {
-});
+test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {});
 
-test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
-    
-});
+test("Executes setEditColor and toggleEdit property when color div is clicked", () => {});
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
-import { render, screen} from "@testing-library/react";
-import ColorList from './ColorList';
+import { render, screen } from "@testing-library/react";
+import ColorList from "./ColorList";
+
+const testColors = [
+  {
+    color: "red",
+    code: { hex: "#225fbg" },
+    id: 42,
+  },
+  { color: "green", code: { hex: "#235300" }, id: 31 },
+];
 
 test("Renders an empty list of colors without errors", () => {
+  render(<ColorList colors={[]} />);
 });
 
 test("Renders a list of colors without errors", () => {
+  render(<ColorList colors={testColors} />);
+  const list = screen.getAllByTestId("color");
+  expect(list).toHaveLength(2);
 });
 
-test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
-});
+test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {});
